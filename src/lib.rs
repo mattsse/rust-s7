3f916155609c1243950e4278960f1f99017ac46a
Matthias <matthias.seitz@tum.de>
2018-04-04 19:44:51 +0200

    updated crates

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,13 @@
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 #![allow(unused)]
 
-extern crate byteorder;
 extern crate bincode;
+extern crate byteorder;
 extern crate chrono;
 
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-
 
 pub mod core;
 pub mod sys;
