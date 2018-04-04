6df009ce4e79fd0100db713d6ba9ef5622717090
Matthias <matthias.seitz@tum.de>
2018-04-04 15:36:16 +0200

    added dependency crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 #![allow(unused)]
+
+extern crate byteorder;
+extern crate bincode;
+extern crate chrono;
+
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+
 pub mod core;
+pub mod sys;
